ae37b0f15575dd6ab163b7ec601b58e367cc82f9
Jon Caneiro <joncaneiro@gmail.com>
2019-10-09 02:34:44 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,24 @@
 class Person
   
+  #getter
+  def name
+    @name
+  end
   
+ #setter
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  #getter
+  def breed 
+    @breed
+  end
+  
+  #setter
+  def breed=(new_breed)
+    @breed = new_breed
+ end
   
   
 end
